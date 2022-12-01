6a3bb8546605ba09b3f46623d2bf590c5c2db25a
Nguyễn Đình Dương <116984839+zuongpro@users.noreply.github.com>
2022-12-01 07:41:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var min = document.querySelector('.min');
   var second = document.querySelector('.second');
 
-  var startDate = new Date(2022, 21, 11);
+  var startDate = new Date(2022, 11, 21);
   days.innerText = Math.floor((new Date - startDate)/86400000);
   countTime();
 
